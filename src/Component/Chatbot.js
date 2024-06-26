0b41ed782659a937cd89be63df8d0828561a27ef
pwanjari23 <pratikshawanjari23@gmail.com>
2024-06-26 15:00:23 +0530

    changes in UI

--- src/Component/Chatbot.js
+++ src/Component/Chatbot.js
@@ -1,9 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { IoIosSend } from 'react-icons/io';
-import { IoChatboxSharp } from "react-icons/io5";
-import { IoMenuSharp } from "react-icons/io5";
-import { IoMoonSharp } from "react-icons/io5";
-import { IoSunnySharp } from "react-icons/io5";
+import { IoChatboxSharp, IoMenuSharp, IoMoonSharp, IoSunnySharp, IoCopyOutline } from "react-icons/io5";
 import axios from 'axios';
 import { marked } from 'marked';
 import chatbot from './Images/chatbot.gif';
@@ -15,6 +12,7 @@ const Chatbot = () => {
     const [darkMode, setDarkMode] = useState(false);
     const [loading, setLoading] = useState(false);
     const [showHiMessage, setShowHiMessage] = useState(true);
+    const [hoveredMessageIndex, setHoveredMessageIndex] = useState(null);
     const messagesEndRef = useRef(null);
 
     const scrollToBottom = () => {
@@ -59,7 +57,7 @@ const Chatbot = () => {
     const renderMessage = (message) => {
         if (message.sender === 'bot') {
             const html = marked(message.text);
-            return <div dangerouslySetInnerHTML={{ __html: html }} />;
+            return <div dangerouslySetInnerHTML={{ __html: html }} className="break-all" />;
         }
         return message.text;
     };
@@ -84,13 +82,20 @@ const Chatbot = () => {
         setSidebarOpen(false)
     };
 
+    const copyToClipboard = (index) => {
+        const textToCopy = messages[index].text.replace(/^[`*]+|[`*]+$/g, '');
+        navigator.clipboard.writeText(textToCopy).then(() => {
+            console.log('Text copied to clipboard:', textToCopy);
+        }).catch((error) => {
+            console.error('Error copying text to clipboard:', error);
+        });
+    };
+
     return (
         <div className={`flex items-center justify-center min-h-screen p-4 ${darkMode ? 'bg-gray-900 text-white' : 'bg-gray-100 text-black'}`}>
             {showHiMessage && (
-                <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2   p-4 z-50">
-                    <img src={chatbot}
-                        className="text-2xl w-48 h-48"
-                    />
+                <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 z-50">
+                    <img src={chatbot} className="text-2xl w-48 h-48" />
                 </div>
             )}
 
@@ -114,26 +119,40 @@ const Chatbot = () => {
 
             <div className="flex flex-col w-full max-w-3xl rounded-lg h-full">
                 {!sidebarOpen && (
-                    <div className={`fixed top-0 left-0 z-50 ${sidebarOpen ? 'ml-16' : 'ml-4'}`}>
+                    <div className={`fixed top-0 left-0 z-50 flex items-center ${sidebarOpen ? 'ml-16' : 'ml-4'}`}>
                         <button className="text-2xl focus:outline-none m-4" onClick={toggleSidebar}>
                             <IoMenuSharp />
                         </button>
                     </div>
-
                 )}
+                <div className='fixed top-2 right-6  flex items-center'>
+                    <h1 className="text-2xl font-semibold ">Talkify</h1>
+                </div>
+
 
                 <div className={`flex-1 overflow-y-auto p-6 ${darkMode ? 'bg-gray-900 text-white' : 'bg-gray-100 text-black'}`}>
                     {messages.map((message, index) => (
-                        <div key={index} className={`my-2 p-2 rounded-lg flex ${message.sender === 'user' ? 'justify-end' : 'justify-start'}`}>
-                            <div className={`rounded-lg p-4 mb-6 max-w-[75%] break-words ${message.sender === 'user' ? (darkMode ? 'bg-gray-700 text-white' : 'bg-blue-500 text-white') : darkMode ? 'bg-gray-800 text-white' : 'bg-gray-200 text-black'}`}>
+                        <div
+                            key={index}
+                            className={`my-2 p-2 rounded-lg flex ${message.sender === 'user' ? 'justify-end' : 'justify-start'}`}
+                            onMouseEnter={() => setHoveredMessageIndex(index)}
+                            onMouseLeave={() => setHoveredMessageIndex(null)}
+                        >
+                            <div className={`relative rounded-lg p-4 mb-6 lg:max-w-[75%] sm:max-w-[75%] break-words ${message.sender === 'user' ? (darkMode ? 'bg-gray-700 text-white' : 'bg-blue-500 text-white') : darkMode ? 'bg-gray-800 text-white' : 'bg-gray-200 text-black'}`}>
                                 {renderMessage(message)}
+                                {hoveredMessageIndex === index && message.sender !== 'user' && (
+                                    <IoCopyOutline
+                                        className="absolute top-1 right-1 text-md cursor-pointer"
+                                        onClick={() => copyToClipboard(index)}
+                                    />
+                                )}
                             </div>
                         </div>
                     ))}
                     {loading && (
                         <div className="my-2 p-2 rounded-lg flex justify-start">
-                            <div className={`rounded-lg p-4 mb-6 max-w-[75%] break-words ${darkMode ? 'text-white' : ' text-black'}`}>
-                                <svg aria-hidden="true" role="status" class="inline w-4 h-4 me-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <div className={`rounded-lg p-4 mb-6 max-w-[75%] sm:max-w-full break-words ${darkMode ? 'text-white' : 'text-black'}`}>
+                                <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
                                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2" />
                                 </svg>
